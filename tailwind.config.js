aa84d8836fe8b67452c8c278eaff6aa6d5862da2
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-15 01:09:24 +0300

    adds extended colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        peach: {
+          500: "#FFD9D4",
+        },
+        mabel: {
+          500: "#D4F6FF",
+        },
+      },
+    },
   },
   plugins: [],
 };
